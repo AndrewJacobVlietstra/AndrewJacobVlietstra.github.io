bfe427edb9c65f8a2b9110e58c6c969e6b3a9a81
Andrew <vlietstra123@hotmail.com>
2021-03-11 16:02:38 -0500

    Made massive changes to the file structure of my portfolio, put all original files into src folder and implemented gulp to compile sass, prefix it and minify it, concatenate my js scripts and minify those and to constantly be watching for changes and copying those into the dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,24 @@
+/*
+Had to do a bunch of crazy stuff to get this to freaking work. I downgraded my nodejs from 15 to 14,
+I also then downgraded node-sass from 5.0.0 to 4.14.0 to fix compatibility issues. I also had to download Python 2.7
+as errors kept getting thrown as node was looking for it. I also got Python 3.9 for good measure though not sure if I need it.
+Was getting weird python permission errors so I had to configure some windows "manage application execution aliases"
+
+*/
+
 const gulp = require('gulp');
+const { series } = require('gulp');
 const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
-const sass = require('gulp-sass');
-const prefix = require('gulp-autoprefixer'); // Autoprefixer, must use: npm install --save-dev gulp-autoprefixer
-const cssmin = require('gulp-cssnano');
+var sass = require('gulp-sass');
+const prefix = require('gulp-autoprefixer'); // autoprefixer, must use: npm install --save-dev gulp-autoprefixer
+const cssmin = require('gulp-cssnano'); // cssnano, must use: npm install --save-dev gulp-cssnano
 const concat = require('gulp-concat');
-const rename = require('gulp-rename');
+const rename = require('gulp-rename'); // rename, must use: npm install --save-dev gulp-rename
+
+
+sass.compiler = require('node-sass');
+
 // const plumber = require('gulp-plumber');
 // const notify = require('gulp-notify');
 // const sassLint = require('gulp-sass-lint');
@@ -32,15 +45,16 @@ gulp.task('message', function() {
 
 
 // Compile Sass, must use: npm install --save-dev gulp-sass
-gulp.task('sass', function(){
+gulp.task('sass', function(done){
     gulp.src('src/sass/*.scss')
-        .pipe(sass())
+        .pipe(sass().on('error', sass.logError))
         .pipe(prefix())
         .pipe(rename('main.css'))
         .pipe(gulp.dest('dist/css'))
         .pipe(cssmin())
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('dist/css'))
+        done();
 });
 
 
@@ -60,41 +74,43 @@ gulp.task('minifyjs', function(){
 });
 
 // Concatenate JS files, must use: npm install --save-dev gulp-concat
-gulp.task('scripts', function(){
+gulp.task('scripts', function(done){
     gulp.src('src/js/*.js')
         .pipe(concat('main.js'))
         .pipe(uglify())
         .pipe(gulp.dest('dist/js'))
+        done();
 });
 
 // Copy HTML
-gulp.task('copyhtml', function(){
+gulp.task('copyhtml', function(done){
     gulp.src('src/*.html')
         .pipe(gulp.dest('dist'))
+        done();
 });
 
 
 // Optimize Images, must use: npm install --save-dev gulp-imagemin
-gulp.task('imageMin', () =>
-    gulp.src('src/images/*')
+gulp.task('imageMin', function(done){
+    gulp.src('src/img/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('dist/images'))
-);
+        .pipe(gulp.dest('dist/img'))
+        done();
+});
 
 // Watch, use: gulp watch
 gulp.task('watch', function(){
-    gulp.watch('src/js/*.js', ['scripts']);
-    gulp.watch('src/images/*', ['imageMin']);
-    gulp.watch('src/sass/*.scss', ['sass']);
-    gulp.watch('src/*.html', ['copyhtml']);
+    gulp.watch('src/js/*.js', gulp.series('scripts'));
+    gulp.watch('src/images/*', gulp.series('imageMin'));
+    gulp.watch('src/sass/*.scss', gulp.series('sass'));
+    gulp.watch('src/*.html', gulp.series('copyhtml'));
 });
 
 /*  BUILD TASKS */
 
-gulp.task('default', function(done) {
-    runSequence('copyhtml', 'sass', 'scripts', 'watch', done);
-});
+// just type gulp in terminal to activate default task!
+gulp.task('default', gulp.series('copyhtml', 'sass', 'scripts', 'watch', function(done) {
+    // task 1 code here
+     done();
+}));
 
-gulp.task('build', function(done) {
-    runSequence('sass', done);
-});
